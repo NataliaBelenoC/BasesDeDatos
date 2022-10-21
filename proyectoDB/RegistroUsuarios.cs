f78797db27444fbc987efa062d629ce10c1288cd
SantiagoP0815 <santiago.p.0815@gmail.com>
2022-10-20 21:40:26 -0500

    Crear registro de usuario

---
+++ proyectoDB/RegistroUsuarios.cs
@@ -0,0 +1,55 @@
+﻿using proyectoDB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoDB
+{
+    public partial class RegistroUsuarios : Form
+    {
+        public RegistroUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Usuarios usuario = new Usuarios();
+            usuario.Usuario = txtUsuario.Text;
+            usuario.Password = txtPassword.Text;
+            usuario.ConPassword = txtConPassword.Text;
+            usuario.Nombre = txtNombre.Text;
+
+            try
+            {
+
+                Control control = new Control();
+                string respuesta = control.ctrlRegistro(usuario);
+
+                if (respuesta.Length > 0)
+                {
+                    MessageBox.Show(respuesta, "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Usuario registrado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
