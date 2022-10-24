26b03949a67bd51e3c7f9013de928f58e510416d
santiago_jagua <santiago.jagua.2021@upb.edu.co>
2022-10-24 00:13:18 -0500

    cambios video

--- proyectoDB/TablaIniciativas.Designer.cs
+++ proyectoDB/TablaIniciativas.Designer.cs
@@ -78,17 +78,17 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(84, 15);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "ID de iniciativa";
             // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(12, 101);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(126, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Text = "buscar comentarios";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
